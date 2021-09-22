120b090639e8459c401e247a4a9df3be99bd3a0a
placidohoff <67982091+placidohoff@users.noreply.github.com>
2021-09-22 19:02:35 -0400

    test commit

---
+++ src/views/Voting/components/TextEllipsis.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { Text } from 'pancake100-uikit'
+
+const TextEllipsis = styled(Text)`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+export default TextEllipsis
